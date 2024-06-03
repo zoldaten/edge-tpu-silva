b38aa64dde44d8508cea7ec432b67cd39985ee5a
Albert <poisk123@yandex.ru>
2024-06-03 16:58:18 +0300

    Update object_detect.py

--- example/object_detect.py
+++ example/object_detect.py
@@ -1,12 +1,77 @@
 from edge_tpu_silva import process_detection
+import cv2,time
+import numpy as np
+from ultralytics import YOLO
 
 # Example Usage with Required Parameters
-model_path = 'path/to/your/model.tflite'
-input_path = 'path/to/your/input/video.mp4'
+#model_path = '240_yolov8n_full_integer_quant_edgetpu.tflite'
+#input_path = 'path/to/your/input/video.mp4'
+#input_path = 'obama.jpg'
 imgsz = 240
 
-# Run the object detection process
-outs = process_detection(model_path, input_path, imgsz)
+from picamera2 import Picamera2, Preview
+camera = Picamera2(tuning="/usr/share/libcamera/ipa/rpi/vc4/ov5647_noir.json")
+    
+preview_config = camera.create_preview_configuration(main={"size": (1280, 720), "format": "RGB888"})
+camera.configure(preview_config)
+camera.start()
+model = YOLO(model='240_yolov8n_full_integer_quant_edgetpu.tflite', task="detect", verbose=False)
 
-for _, _ in outs:
-  pass
+while 1:
+    np_array=camera.capture_array()
+    input_path = np_array[:,:,:3]
+    #image = cv2.resize(np_array, (240, 240))
+
+    #input_path = np.expand_dims(np_array, axis=0)
+
+    # Run the object detection process
+    start_time = time.time()
+    outs = model.predict(
+        source=input_path,
+        conf=0.5,
+        imgsz=imgsz,
+        verbose=False,
+        stream=False,
+        show=True,    )
+    #outs = process_detection(model_path, input_path, imgsz, show=True)
+    
+    
+    #frame_count = 0
+    start_time = time.time()
+    for out in outs:
+        
+        objs_lst = []
+        for box in out.boxes:
+            obj_cls, conf, bb = (
+                box.cls.numpy()[0],
+                box.conf.numpy()[0],
+                box.xyxy.numpy()[0],
+            )
+            label = out.names[int(obj_cls)]
+            ol = {
+                "id": obj_cls,
+                "label": label,
+                "conf": conf,
+                "bbox": bb,
+            }
+            objs_lst.append(ol)
+
+            
+            print(label)
+            #print("  id:    ", obj_cls)
+            #print("  score: ", conf)
+            #print("  bbox:  ", bb)
+
+        #frame_count += 1
+        #elapsed_time = time.time() - start_time
+        #fps = frame_count / elapsed_time
+
+        #print (objs_lst, fps)
+        print (objs_lst)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+        # Break the loop if 'esc' key is pressed for video or camera
+        if cv2.waitKey(1) == 27:
+            break
+    #for _, _ in outs:
+      #pass
